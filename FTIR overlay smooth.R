ad85fa51ed64515771a25362157a75bf12a75cd2
John Merston <121535867+Johnmerston@users.noreply.github.com>
2024-08-06 09:37:25 +0530

    Update FTIR overlay smooth.R

--- FTIR overlay smooth.R
+++ FTIR overlay smooth.R
@@ -1,16 +1,26 @@
-library(signal)
+#The following libraries are required for an FTIR plot
+#The function of readxl is to import a .xlsx file into Rstudio - Use this library only if you have your date stored in a .xlsx file. 
+#for data in form of a .txt file, upload the data into an excel sheet and then continue with readxl import 
+library(readxl)
+
+#ggplot2 is used for plotting the data obtained post processing through signal package
+#tidyr package is used in this case for converting more than one line plot data into one single plot
+#signal package is used to process the FTIR data - primarily for savitsky golay smoothening - Do read on window size and poly order if mentioned in any research article
 library(ggplot2)
 library(tidyr)
-library(readxl)
+library(signal)
+
 
 # Reading the data from the provided file
 Test_file <- read_xlsx("file_name.xlsx")
 
 # Converting the data to a data frame and ensuring column names are correct
+#Here X represents the Wave number that is to be plotted along the X axis and CM_s represent the Y axis values for 4 FTIR plots
 colnames(Test_file) <- c("X", "CM_1", "CM_2", "CM_3", "CM_4")
 Test_df <- as.data.frame(Test_file)
 
 # Ensuring X and CM_* columns are numeric
+#This step is done to ensure that Rstudio recognizes all values as numbers
 Test_df$X <- as.numeric(Test_df$X)
 Test_df$CM_1 <- as.numeric(Test_df$CM_1)
 Test_df$CM_2 <- as.numeric(Test_df$CM_2)
@@ -18,21 +28,25 @@ Test_df$CM_3 <- as.numeric(Test_df$CM_3)
 Test_df$CM_4 <- as.numeric(Test_df$CM_4)
 
 # Define window size and polynomial order
+#Do follow recommendations given in reference articles but one can always customize this if necessary
+#Do note that window size must be an odd number
 window_size <- 21
 poly_order <- 3
 
 # Apply Savitzky-Golay smoothing
+#While there are other types of smoothening methods, majority of research articles follow this
 Test_df$CM_smooth_1 <- sgolayfilt(Test_df$CM_1, p = poly_order, n = window_size)
 Test_df$CM_smooth_2 <- sgolayfilt(Test_df$CM_2, p = poly_order, n = window_size)
 Test_df$CM_smooth_3 <- sgolayfilt(Test_df$CM_3, p = poly_order, n = window_size)
 Test_df$CM_smooth_4 <- sgolayfilt(Test_df$CM_4, p = poly_order, n = window_size)
 
+#Converting file names (step no necessary)
 Test_df$CM_H1 <- Test_df$CM_smooth_1
 Test_df$CM_H2 <- Test_df$CM_smooth_2
 Test_df$CM_H3 <- Test_df$CM_smooth_3
 Test_df$CM_H4 <- Test_df$CM_smooth_4
 
-# Transform the data into a long format
+# Transform the data into a long format - Combines all four FTIR data into one - Can be done for more
 data_long <- pivot_longer(Test_df, cols = starts_with("CM_H"), names_to = "Series", values_to = "Intensity")
 
 # Create the plot
